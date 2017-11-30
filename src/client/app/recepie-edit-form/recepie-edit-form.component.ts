d94197cad9a996bec76711d3bee001a91bf9ddf8
Pawel Maricnkowski <wafel34@gmail.com>
2017-11-30 22:38:05 +0000

    Added basic funcionality for FormArray for ingredients

--- src/client/app/recepie-edit-form/recepie-edit-form.component.ts
+++ src/client/app/recepie-edit-form/recepie-edit-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from '../shared/api.service';
 import { Recepie } from '../shared/recepie.model';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-recepie-edit-form',
@@ -12,11 +12,13 @@ export class RecepieEditFormComponent implements OnInit {
 
     @Input() recepie: Recepie;
     recepiesForm: FormGroup;
+    ingredients: any[] = [];
 
     constructor(private formBuilder: FormBuilder) {
     }
 
     ngOnInit() {
+
         this.recepiesForm = this.formBuilder.group({
             name: [this.recepie.name],
             shortname: [this.recepie.shortName],
@@ -25,10 +27,30 @@ export class RecepieEditFormComponent implements OnInit {
             category: [this.recepie.category],
             time: [this.recepie.time],
             serves: [this.recepie.serves],
-            ingredients: [this.recepie.ingredients],
+            ingredients: this.formBuilder.array([]),
             instructions: [this.recepie.instructions],
             photoUrl: [this.recepie.photoUrl]
         });
+
+        this.initializeIngridients();
     }
+
+
+    initializeIngridients() {
+        this.ingredients = this.formBuilder.array(this.recepie.ingredients);
+        this.recepiesForm.setControl('ingredients', this.ingredients);
+    }
+
+    addIngredient() {
+        this.ingredients = this.recepiesForm.get('ingredients');
+        this.ingredients.push(this.formBuilder.control(''));
+    }
+
+    onSubmit() {
+        console.log('dyao', this.recepiesForm);
+    }
+
+
+
 
 }
