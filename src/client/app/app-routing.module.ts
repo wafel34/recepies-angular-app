e903801b319977bc2d785d87e14a9d6e085e1dc6
Pawel Maricnkowski <wafel34@gmail.com>
2017-12-10 11:38:41 +0000

    Started with styling menu

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecepiesListComponent } from './recepies-list/recepies-list.component';
 import { RecepiePageComponent } from './recepie-page/recepie-page.component';
+import { RecepieEditFormComponent } from './recepie-edit-form/recepie-edit-form.component';
 
 const routes: Routes = [
     {path: 'recepies', component: RecepiesListComponent},
+    {path: 'home', component: RecepiesListComponent},
+    {path: 'add', component: RecepieEditFormComponent},
     {path: 'recepies/:shortname', component: RecepiePageComponent}
+
 ];
 
 @NgModule({
