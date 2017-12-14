55a2e536ec10e45af02a95be7c6c222967e3069e
Pawel Marcinkowski <pmarcinkowski1016@timeinc.com>
2017-12-14 12:26:15 +0000

    Finished with styling base layout for now, time for user authentication

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RecepiePageComponent } from './recepie-page/recepie-page.component';
 import { RecepieEditFormComponent } from './recepie-edit-form/recepie-edit-form.component';
 
 const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'recepies', component: RecepiesListComponent},
     {path: 'home', component: RecepiesListComponent},
     {path: 'add', component: RecepieEditFormComponent},
