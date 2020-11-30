2d27127158fc5bf1fb733fc0f0c9a789a046a18f
wafel <wafel34@gmail.com>
2020-11-30 23:10:48 +0100

    migrate db

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-const databaseURL = "mongodb://"+process.env.USER+":"+process.env.PASSWORD+"@ds115198.mlab.com:15198/recipes-angular-db";
+// const databaseURL = "mongodb://"+process.env.USER+":"+process.env.PASSWORD+"@ds115198.mlab.com:15198/recipes-angular-db";
+// const databaseURL = "mongodb+srv://"+process.env.USER+":"+process.env.PASSWORD+"@recipes-angular-db.noc3w.mongodb.net/recipes-angular-db?retryWrites=true&w=majority";
+const databaseURL = "mongodb://"+process.env.USER+":"+process.env.PASSWORD+"@recipes-angular-db-shard-00-00.noc3w.mongodb.net:27017,recipes-angular-db-shard-00-01.noc3w.mongodb.net:27017,recipes-angular-db-shard-00-02.noc3w.mongodb.net:27017/recipes-angular-db?ssl=true&replicaSet=atlas-st29w3-shard-0&authSource=admin&retryWrites=true&w=majority";
 const createExpressApp = require('./create-express-app');
 
 MongoClient.connect(databaseURL, (err, db) => {
